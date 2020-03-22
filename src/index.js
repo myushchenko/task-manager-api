d95e0b8588d784b24d76aff7d4351d10e45ec821
Mykhailo Yushchenko <mykhailo.yuschenko@ihsmarkit.com>
2020-03-22 13:43:38 +0200

    Init commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./routes/user');
+const taskRouter = require('./routes/task');
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(PORT, () => {
+	console.log('Server is up on port', PORT);
+});
